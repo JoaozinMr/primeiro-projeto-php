e41725a56dc6009e323daadcba4297c9b356b170
JoaozinMr <joao.florv2005@gmail.com>
2023-03-02 18:10:31 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "Ola mundo"?></h1>
+
+    <h2> <p>10 + 5 = <?php echo(10 + 5)?></p> </h2>
+
+    <h2> <p>20 - 2  = <?php echo(20-2)?></p> </h2>
+
+    <h2> <p>15 * 3 = <?php echo(15*3)?></p> </h2>
+
+    <h2> <p>8 / 2 = <?php echo(8/2)?></p> </h2>
+</body>
+</html>
